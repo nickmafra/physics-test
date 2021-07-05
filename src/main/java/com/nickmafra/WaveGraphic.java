36a47b748de06a3207932ad28fc28c6a18fbfec6
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2021-07-05 17:09:44 -0300

    Fix ao remover a dependência nm-concurrent

--- src/main/java/com/nickmafra/WaveGraphic.java
+++ src/main/java/com/nickmafra/WaveGraphic.java
@@ -1,23 +1,26 @@
 package com.nickmafra;
 
-import com.nickmafra.gfx.LimitedRateThread;
 import com.nickmafra.gfx.PixelDrawer;
 import com.nickmafra.gfx.SimpleGraphic;
 
 import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class WaveGraphic extends SimpleGraphic {
 
     private final Wave wave;
-    private final LimitedRateThread updateThead;
+    private final double fps;
+    private final ScheduledExecutorService executor;
 
     public WaveGraphic(Wave wave, int scale, double fps) {
         super("Wave", wave.getWidth() * scale, wave.getHeight() * scale, fps);
         this.wave = wave;
+        this.fps = fps;
 
-        this.updateThead = new LimitedRateThread((long) (1000 / fps), wave::update);
-
-        addOnClosingListener(e -> this.updateThead.interrupt());
+        executor = Executors.newSingleThreadScheduledExecutor();
+        addOnClosingListener(e -> executor.shutdownNow());
 
         PixelDrawer pixelDrawer = new PixelDrawer(wave.getWidth(), wave.getHeight(), scale, this::getColor);
         setDrawer(pixelDrawer::draw);
@@ -30,8 +33,14 @@ public class WaveGraphic extends SimpleGraphic {
     }
 
     @Override
+    public void close() {
+        executor.shutdownNow();
+        super.close();
+    }
+
+    @Override
     public void start() {
         super.start();
-        updateThead.start();
+        this.executor.scheduleWithFixedDelay(wave::update, 0, (long) (1000 / fps), TimeUnit.MILLISECONDS);
     }
 }
